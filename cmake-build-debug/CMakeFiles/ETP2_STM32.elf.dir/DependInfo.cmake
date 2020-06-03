a68463472ce23bda1625729a24c90c686b95b5f1
Manuel Schmid (schmim62) <schmim62@students.zhaw.ch>
2020-06-03 22:30:48 +0200

    Revert "Clean up part 1"
    This reverts commit 7793f9121e4583fb778c6d13bcf72223dda14d34.

---
+++ cmake-build-debug/CMakeFiles/ETP2_STM32.elf.dir/DependInfo.cmake
@@ -0,0 +1,91 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "C:/Users/Manuel/Documents/GitHub/ETP2_STM32/Core/Startup/startup_stm32f030c8tx.s" "C:/Users/Manuel/Documents/GitHub/ETP2_STM32/cmake-build-debug/CMakeFiles/ETP2_STM32.elf.dir/Core/Startup/startup_stm32f030c8tx.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "DEBUG"
+  "STM32F030x8"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "../Drivers/STM32F0xx_HAL_Driver/Inc"
+  "../Drivers/CMSIS/Include"
+  "../Core/Inc"
+  "../Drivers/STM32F0xx_HAL_Driver/Inc/Legacy"
+  "../Drivers/CMSIS/Device/ST/STM32F0xx/Include"
+  )
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/Manuel/Documents/GitHub/ETP2_STM32/Core/Src/action_handler.c" "C:/Users/Manuel/Documents/GitHub/ETP2_STM32/cmake-build-debug/CMakeFiles/ETP2_STM32.elf.dir/Core/Src/action_handler.c.obj"
+  "C:/Users/Manuel/Documents/GitHub/ETP2_STM32/Core/Src/adc.c" "C:/Users/Manuel/Documents/GitHub/ETP2_STM32/cmake-build-debug/CMakeFiles/ETP2_STM32.elf.dir/Core/Src/adc.c.obj"
+  "C:/Users/Manuel/Documents/GitHub/ETP2_STM32/Core/Src/bitmaps.c" "C:/Users/Manuel/Documents/GitHub/ETP2_STM32/cmake-build-debug/CMakeFiles/ETP2_STM32.elf.dir/Core/Src/bitmaps.c.obj"
+  "C:/Users/Manuel/Documents/GitHub/ETP2_STM32/Core/Src/event_handler.c" "C:/Users/Manuel/Documents/GitHub/ETP2_STM32/cmake-build-debug/CMakeFiles/ETP2_STM32.elf.dir/Core/Src/event_handler.c.obj"
+  "C:/Users/Manuel/Documents/GitHub/ETP2_STM32/Core/Src/font.c" "C:/Users/Manuel/Documents/GitHub/ETP2_STM32/cmake-build-debug/CMakeFiles/ETP2_STM32.elf.dir/Core/Src/font.c.obj"
+  "C:/Users/Manuel/Documents/GitHub/ETP2_STM32/Core/Src/gpio.c" "C:/Users/Manuel/Documents/GitHub/ETP2_STM32/cmake-build-debug/CMakeFiles/ETP2_STM32.elf.dir/Core/Src/gpio.c.obj"
+  "C:/Users/Manuel/Documents/GitHub/ETP2_STM32/Core/Src/i2c.c" "C:/Users/Manuel/Documents/GitHub/ETP2_STM32/cmake-build-debug/CMakeFiles/ETP2_STM32.elf.dir/Core/Src/i2c.c.obj"
+  "C:/Users/Manuel/Documents/GitHub/ETP2_STM32/Core/Src/lcd_st7565.c" "C:/Users/Manuel/Documents/GitHub/ETP2_STM32/cmake-build-debug/CMakeFiles/ETP2_STM32.elf.dir/Core/Src/lcd_st7565.c.obj"
+  "C:/Users/Manuel/Documents/GitHub/ETP2_STM32/Core/Src/main.c" "C:/Users/Manuel/Documents/GitHub/ETP2_STM32/cmake-build-debug/CMakeFiles/ETP2_STM32.elf.dir/Core/Src/main.c.obj"
+  "C:/Users/Manuel/Documents/GitHub/ETP2_STM32/Core/Src/rtc.c" "C:/Users/Manuel/Documents/GitHub/ETP2_STM32/cmake-build-debug/CMakeFiles/ETP2_STM32.elf.dir/Core/Src/rtc.c.obj"
+  "C:/Users/Manuel/Documents/GitHub/ETP2_STM32/Core/Src/state_machine.c" "C:/Users/Manuel/Documents/GitHub/ETP2_STM32/cmake-build-debug/CMakeFiles/ETP2_STM32.elf.dir/Core/Src/state_machine.c.obj"
+  "C:/Users/Manuel/Documents/GitHub/ETP2_STM32/Core/Src/stm32f0xx_hal_msp.c" "C:/Users/Manuel/Documents/GitHub/ETP2_STM32/cmake-build-debug/CMakeFiles/ETP2_STM32.elf.dir/Core/Src/stm32f0xx_hal_msp.c.obj"
+  "C:/Users/Manuel/Documents/GitHub/ETP2_STM32/Core/Src/stm32f0xx_it.c" "C:/Users/Manuel/Documents/GitHub/ETP2_STM32/cmake-build-debug/CMakeFiles/ETP2_STM32.elf.dir/Core/Src/stm32f0xx_it.c.obj"
+  "C:/Users/Manuel/Documents/GitHub/ETP2_STM32/Core/Src/syscalls.c" "C:/Users/Manuel/Documents/GitHub/ETP2_STM32/cmake-build-debug/CMakeFiles/ETP2_STM32.elf.dir/Core/Src/syscalls.c.obj"
+  "C:/Users/Manuel/Documents/GitHub/ETP2_STM32/Core/Src/sysmem.c" "C:/Users/Manuel/Documents/GitHub/ETP2_STM32/cmake-build-debug/CMakeFiles/ETP2_STM32.elf.dir/Core/Src/sysmem.c.obj"
+  "C:/Users/Manuel/Documents/GitHub/ETP2_STM32/Core/Src/system_stm32f0xx.c" "C:/Users/Manuel/Documents/GitHub/ETP2_STM32/cmake-build-debug/CMakeFiles/ETP2_STM32.elf.dir/Core/Src/system_stm32f0xx.c.obj"
+  "C:/Users/Manuel/Documents/GitHub/ETP2_STM32/Core/Src/tim.c" "C:/Users/Manuel/Documents/GitHub/ETP2_STM32/cmake-build-debug/CMakeFiles/ETP2_STM32.elf.dir/Core/Src/tim.c.obj"
+  "C:/Users/Manuel/Documents/GitHub/ETP2_STM32/Core/Src/usart.c" "C:/Users/Manuel/Documents/GitHub/ETP2_STM32/cmake-build-debug/CMakeFiles/ETP2_STM32.elf.dir/Core/Src/usart.c.obj"
+  "C:/Users/Manuel/Documents/GitHub/ETP2_STM32/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal.c" "C:/Users/Manuel/Documents/GitHub/ETP2_STM32/cmake-build-debug/CMakeFiles/ETP2_STM32.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal.c.obj"
+  "C:/Users/Manuel/Documents/GitHub/ETP2_STM32/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_adc.c" "C:/Users/Manuel/Documents/GitHub/ETP2_STM32/cmake-build-debug/CMakeFiles/ETP2_STM32.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_adc.c.obj"
+  "C:/Users/Manuel/Documents/GitHub/ETP2_STM32/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_adc_ex.c" "C:/Users/Manuel/Documents/GitHub/ETP2_STM32/cmake-build-debug/CMakeFiles/ETP2_STM32.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_adc_ex.c.obj"
+  "C:/Users/Manuel/Documents/GitHub/ETP2_STM32/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_cortex.c" "C:/Users/Manuel/Documents/GitHub/ETP2_STM32/cmake-build-debug/CMakeFiles/ETP2_STM32.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_cortex.c.obj"
+  "C:/Users/Manuel/Documents/GitHub/ETP2_STM32/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_dma.c" "C:/Users/Manuel/Documents/GitHub/ETP2_STM32/cmake-build-debug/CMakeFiles/ETP2_STM32.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_dma.c.obj"
+  "C:/Users/Manuel/Documents/GitHub/ETP2_STM32/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_exti.c" "C:/Users/Manuel/Documents/GitHub/ETP2_STM32/cmake-build-debug/CMakeFiles/ETP2_STM32.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_exti.c.obj"
+  "C:/Users/Manuel/Documents/GitHub/ETP2_STM32/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_flash.c" "C:/Users/Manuel/Documents/GitHub/ETP2_STM32/cmake-build-debug/CMakeFiles/ETP2_STM32.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_flash.c.obj"
+  "C:/Users/Manuel/Documents/GitHub/ETP2_STM32/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_flash_ex.c" "C:/Users/Manuel/Documents/GitHub/ETP2_STM32/cmake-build-debug/CMakeFiles/ETP2_STM32.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_flash_ex.c.obj"
+  "C:/Users/Manuel/Documents/GitHub/ETP2_STM32/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_gpio.c" "C:/Users/Manuel/Documents/GitHub/ETP2_STM32/cmake-build-debug/CMakeFiles/ETP2_STM32.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_gpio.c.obj"
+  "C:/Users/Manuel/Documents/GitHub/ETP2_STM32/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_i2c.c" "C:/Users/Manuel/Documents/GitHub/ETP2_STM32/cmake-build-debug/CMakeFiles/ETP2_STM32.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_i2c.c.obj"
+  "C:/Users/Manuel/Documents/GitHub/ETP2_STM32/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_i2c_ex.c" "C:/Users/Manuel/Documents/GitHub/ETP2_STM32/cmake-build-debug/CMakeFiles/ETP2_STM32.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_i2c_ex.c.obj"
+  "C:/Users/Manuel/Documents/GitHub/ETP2_STM32/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_pwr.c" "C:/Users/Manuel/Documents/GitHub/ETP2_STM32/cmake-build-debug/CMakeFiles/ETP2_STM32.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_pwr.c.obj"
+  "C:/Users/Manuel/Documents/GitHub/ETP2_STM32/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_pwr_ex.c" "C:/Users/Manuel/Documents/GitHub/ETP2_STM32/cmake-build-debug/CMakeFiles/ETP2_STM32.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_pwr_ex.c.obj"
+  "C:/Users/Manuel/Documents/GitHub/ETP2_STM32/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_rcc.c" "C:/Users/Manuel/Documents/GitHub/ETP2_STM32/cmake-build-debug/CMakeFiles/ETP2_STM32.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_rcc.c.obj"
+  "C:/Users/Manuel/Documents/GitHub/ETP2_STM32/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_rcc_ex.c" "C:/Users/Manuel/Documents/GitHub/ETP2_STM32/cmake-build-debug/CMakeFiles/ETP2_STM32.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_rcc_ex.c.obj"
+  "C:/Users/Manuel/Documents/GitHub/ETP2_STM32/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_rtc.c" "C:/Users/Manuel/Documents/GitHub/ETP2_STM32/cmake-build-debug/CMakeFiles/ETP2_STM32.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_rtc.c.obj"
+  "C:/Users/Manuel/Documents/GitHub/ETP2_STM32/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_rtc_ex.c" "C:/Users/Manuel/Documents/GitHub/ETP2_STM32/cmake-build-debug/CMakeFiles/ETP2_STM32.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_rtc_ex.c.obj"
+  "C:/Users/Manuel/Documents/GitHub/ETP2_STM32/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_tim.c" "C:/Users/Manuel/Documents/GitHub/ETP2_STM32/cmake-build-debug/CMakeFiles/ETP2_STM32.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_tim.c.obj"
+  "C:/Users/Manuel/Documents/GitHub/ETP2_STM32/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_tim_ex.c" "C:/Users/Manuel/Documents/GitHub/ETP2_STM32/cmake-build-debug/CMakeFiles/ETP2_STM32.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_tim_ex.c.obj"
+  "C:/Users/Manuel/Documents/GitHub/ETP2_STM32/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_uart.c" "C:/Users/Manuel/Documents/GitHub/ETP2_STM32/cmake-build-debug/CMakeFiles/ETP2_STM32.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_uart.c.obj"
+  "C:/Users/Manuel/Documents/GitHub/ETP2_STM32/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_uart_ex.c" "C:/Users/Manuel/Documents/GitHub/ETP2_STM32/cmake-build-debug/CMakeFiles/ETP2_STM32.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_uart_ex.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "DEBUG"
+  "STM32F030x8"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../Drivers/STM32F0xx_HAL_Driver/Inc"
+  "../Drivers/CMSIS/Include"
+  "../Core/Inc"
+  "../Drivers/STM32F0xx_HAL_Driver/Inc/Legacy"
+  "../Drivers/CMSIS/Device/ST/STM32F0xx/Include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
