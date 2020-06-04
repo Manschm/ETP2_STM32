87e5d2f4b80a58192fc05e8bfea66e9d5a6c1b78
Manuel Schmid (schmim62) <schmim62@students.zhaw.ch>
2020-06-04 14:34:57 +0200

    UART stuff

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -45,10 +45,17 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
+// PWM defines
 #define PWM_CH_R    TIM_CHANNEL_1
 #define PWM_CH_G    TIM_CHANNEL_2
 #define PWM_CH_B    TIM_CHANNEL_3
 #define PWM_CH_W    TIM_CHANNEL_4
+
+// UART defines
+#define UART_DATA_LEN   5U
+
+// Interrupt flags
+#define FLAG_UART 0U
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -60,16 +67,21 @@
 
 /* USER CODE BEGIN PV */
 
-// Interrupt variables
+// UART variables
+uint8_t uartRxData[UART_DATA_LEN];
+
+/*===== Interrupt flags
+ * Bit | Description
+ *   0 | New UART data available
+ *   1 | Temperature sensor TODO: define
+ */
 //static uint8_t updateUI = 0;
 //static uint8_t updateSensor = 0;
+volatile uint8_t updateFlags = 0;
 
 // Data buffer
-uint8_t shtc3RxBuf[6];
-uint8_t shtc3TxBuf[2];
-
-// Flags
-uint8_t readSHTC3 = 0;
+//uint8_t shtc3RxBuf[6];
+//uint8_t shtc3TxBuf[2];
 
 // LCD buffer
 uint8_t LCD_Buffer[1024];
@@ -94,41 +106,42 @@ void SystemClock_Config(void);
   * @brief  The application entry point.
   * @retval int
   */
-int main(void)
-{
-  /* USER CODE BEGIN 1 */
+int main(void) {
+    /* USER CODE BEGIN 1 */
 
-  /* USER CODE END 1 */
+    /* USER CODE END 1 */
 
-  /* MCU Configuration--------------------------------------------------------*/
+    /* MCU Configuration--------------------------------------------------------*/
 
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-  HAL_Init();
+    /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+    HAL_Init();
 
-  /* USER CODE BEGIN Init */
+    /* USER CODE BEGIN Init */
 
-  /* USER CODE END Init */
+    /* USER CODE END Init */
 
-  /* Configure the system clock */
-  SystemClock_Config();
+    /* Configure the system clock */
+    SystemClock_Config();
 
-  /* USER CODE BEGIN SysInit */
+    /* USER CODE BEGIN SysInit */
 
-  /* USER CODE END SysInit */
+    /* USER CODE END SysInit */
 
-  /* Initialize all configured peripherals */
-  MX_GPIO_Init();
-  MX_DMA_Init();
-  MX_RTC_Init();
-  MX_TIM1_Init();
-  MX_TIM15_Init();
-  MX_USART1_UART_Init();
-  MX_ADC_Init();
-  MX_I2C2_Init();
-  MX_TIM3_Init();
-  MX_TIM6_Init();
-  MX_TIM14_Init();
-  /* USER CODE BEGIN 2 */
+    /* Initialize all configured peripherals */
+    MX_GPIO_Init();
+    MX_DMA_Init();
+    MX_RTC_Init();
+    MX_TIM1_Init();
+    MX_TIM15_Init();
+    MX_USART1_UART_Init();
+    MX_ADC_Init();
+    MX_I2C2_Init();
+    MX_TIM3_Init();
+    MX_TIM6_Init();
+    MX_TIM14_Init();
+    /* USER CODE BEGIN 2 */
+
+    HAL_UART_Receive_DMA(&huart1, uartRxData, UART_DATA_LEN);
 
     HAL_RTC_DeactivateAlarm(&hrtc, RTC_ALARM_A);
 
@@ -185,71 +198,112 @@ int main(void)
     HAL_RTC_GetAlarm(&hrtc, &sAlarm, RTC_ALARM_A, RTC_FORMAT_BCD);
     HAL_RTC_SetAlarm_IT(&hrtc, &sAlarm, RTC_FORMAT_BCD);
 
-  /* USER CODE END 2 */
+    /* USER CODE END 2 */
 
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
+    /* Infinite loop */
+    /* USER CODE BEGIN WHILE */
 #pragma clang diagnostic push
 #pragma ide diagnostic ignored "EndlessLoop"
     while (1) {
-    /* USER CODE END WHILE */
 
-    /* USER CODE BEGIN 3 */
+        event = eh_get_event();
+        if (event != EV_NO_EVENT) {
+            fsm_handle_event(event);
+        }
+
+        if (updateFlags & (1<<FLAG_UART)) {
+            float brightness = uartRxData[0] / 100.0f;
+            uint8_t lightSwitch = uartRxData[1];
+            uint8_t red = uartRxData[2];
+            uint8_t green = uartRxData[3];
+            uint8_t blue = uartRxData[4];
+            uint8_t white;
+
+            uint16_t rgbwValues[4];
+            uint8_t i;
+
+            //===== Convert RGB to RGBW
+            if (red <= green) {
+                if (red <= blue) {          // red <= (green && blue)
+                    white = red;
+                } else {                    // blue < red <= green
+                    white = blue;
+                }
+            } else if (green <= blue) {     // green <= (red && blue)
+                white = green;
+            } else {                        // blue < green < red
+                white = blue;
+            }
+
+            rgbwValues[0] = red - white;
+            rgbwValues[1] = green - white;
+            rgbwValues[2] = blue - white;
+            rgbwValues[3] = white;
+
+            //===== Adjust brightness
+
+            for (i=0; i<4; i++) {
+                rgbwValues[i] = (rgbwValues * 5) >> 1;  // Adjust scale (max. value 255 becomes 637)
+                rgbwValues[i] *= brightness;            // Scale with brightness
+            }
+        }
+
+
+
+        /* USER CODE END WHILE */
+
+        /* USER CODE BEGIN 3 */
     }
 #pragma clang diagnostic pop
 }
-  /* USER CODE END 3 */
+/* USER CODE END 3 */
 }
 
 /**
   * @brief System Clock Configuration
   * @retval None
   */
-void SystemClock_Config(void)
-{
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
-  RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
+void SystemClock_Config(void) {
+    RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+    RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+    RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
 
-  /** Configure LSE Drive Capability 
-  */
-  HAL_PWR_EnableBkUpAccess();
-  __HAL_RCC_LSEDRIVE_CONFIG(RCC_LSEDRIVE_HIGH);
-  /** Initializes the CPU, AHB and APB busses clocks 
-  */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI14|RCC_OSCILLATORTYPE_HSE
-                              |RCC_OSCILLATORTYPE_LSE;
-  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
-  RCC_OscInitStruct.LSEState = RCC_LSE_ON;
-  RCC_OscInitStruct.HSI14State = RCC_HSI14_ON;
-  RCC_OscInitStruct.HSI14CalibrationValue = 16;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL6;
-  RCC_OscInitStruct.PLL.PREDIV = RCC_PREDIV_DIV1;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /** Initializes the CPU, AHB and APB busses clocks 
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+    /** Configure LSE Drive Capability
+    */
+    HAL_PWR_EnableBkUpAccess();
+    __HAL_RCC_LSEDRIVE_CONFIG(RCC_LSEDRIVE_HIGH);
+    /** Initializes the CPU, AHB and APB busses clocks
+    */
+    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI14 | RCC_OSCILLATORTYPE_HSE
+                                       | RCC_OSCILLATORTYPE_LSE;
+    RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+    RCC_OscInitStruct.LSEState = RCC_LSE_ON;
+    RCC_OscInitStruct.HSI14State = RCC_HSI14_ON;
+    RCC_OscInitStruct.HSI14CalibrationValue = 16;
+    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+    RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL6;
+    RCC_OscInitStruct.PLL.PREDIV = RCC_PREDIV_DIV1;
+    if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
+        Error_Handler();
+    }
+    /** Initializes the CPU, AHB and APB busses clocks
+    */
+    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK
+                                  | RCC_CLOCKTYPE_PCLK1;
+    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USART1|RCC_PERIPHCLK_RTC;
-  PeriphClkInit.Usart1ClockSelection = RCC_USART1CLKSOURCE_PCLK1;
-  PeriphClkInit.RTCClockSelection = RCC_RTCCLKSOURCE_LSE;
-  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
-  {
-    Error_Handler();
-  }
+    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK) {
+        Error_Handler();
+    }
+    PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USART1 | RCC_PERIPHCLK_RTC;
+    PeriphClkInit.Usart1ClockSelection = RCC_USART1CLKSOURCE_PCLK1;
+    PeriphClkInit.RTCClockSelection = RCC_RTCCLKSOURCE_LSE;
+    if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK) {
+        Error_Handler();
+    }
 }
 
 /* USER CODE BEGIN 4 */
@@ -276,18 +330,23 @@ void HAL_RTC_AlarmAEventCallback(RTC_HandleTypeDef *hrtc) {
         }
     }
 }
+
+void UART_DMAReceiveCplt(DMA_HandleTypeDef *hdma) {
+    if (hdma->Instance == USART1) {
+        updateFlags |= (1<<FLAG_UART);
+    }
+}
 /* USER CODE END 4 */
 
 /**
   * @brief  This function is executed in case of error occurrence.
   * @retval None
   */
-void Error_Handler(void)
-{
-  /* USER CODE BEGIN Error_Handler_Debug */
+void Error_Handler(void) {
+    /* USER CODE BEGIN Error_Handler_Debug */
     /* User can add his own implementation to report the HAL error return state */
 
-  /* USER CODE END Error_Handler_Debug */
+    /* USER CODE END Error_Handler_Debug */
 }
 
 #ifdef  USE_FULL_ASSERT
